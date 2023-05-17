47b8e71bfe115c12ed66927927404f66bf9f72ef
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-05-17 13:53:36 +0200

    add the log record

--- done_ardupilot.py
+++ done_ardupilot.py
@@ -4,9 +4,49 @@ from pymavlink import mavutil # Needed for command message definitions
 import math
 import time
 import argparse
+import os
+import datetime
 from expan import DIR_VECTORS
+import inspect
+
+# Declare global variables for logs 
+filename = " "
+directory= " "
+log_file_name= " "
+
+def create_log_file(dir,script_name ):
+    # Get the name of the script using this module
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Create the log file name
+    global filename
+    filename = f"{script_name}_{timestamp}.log"
+    
+    # Set the global directory variable
+    global directory
+    directory = dir
+
+def open_log_file():
+    # Get the absolute path of the log file
+    log_file_path = os.path.join(directory, filename)
+    # Open the log file
+    return open(log_file_path, 'a') # dont use "w" because it remove all what was before after each open
+
+def write_log_message(message):
+    with open_log_file() as log_file:
+        timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+        log_file.write(f"{timestamp}: {message}\n")
+
+def get_current_function_name():
+    # Get the frame of the calling function
+    frame = inspect.currentframe().f_back
+    # Get the name of the calling function from the frame
+    function_name = frame.f_code.co_name
+    return function_name
+
 
 def parse_connect():
+    write_log_message (f"{get_current_function_name()} called:") 
     parser=argparse.ArgumentParser (description='commands')
     parser.add_argument('--connect')
     args = parser.parse_args()
@@ -16,19 +56,24 @@ def parse_connect():
     return connection_string
 
 def arm_and_takeoff(self, aTargetAltitude):
+    write_log_message (f"{get_current_function_name()} called:") 
+
     """
     Arms vehicle and fly to aTargetAltitude.
     """
     if self.mode.name == "INITIALISING":
         print ("initialise the self") 
+        write_log_message ("initialise the self") 
 
     print("Basic pre-arm checks") 
     # Don't try to arm until autopilot is ready
     while not self.is_armable:
         print ("Waiting for self to initialise...")
+        write_log_message ("Waiting for self to initialise...") 
         time.sleep(3)
 
     print ("Arming motors")
+    write_log_message ("Arming motors")
     # Copter should arm in GUIDED mode
     self.mode    = VehicleMode("GUIDED")
     self.armed   = True
@@ -36,9 +81,11 @@ def arm_and_takeoff(self, aTargetAltitude):
     # Confirm vehicle armed before attempting to take off
     while not self.armed:
         print (" Waiting for arming...")
+        write_log_message (" Waiting for arming...")
         time.sleep(1)
 
     print ("Taking off starts") 
+    write_log_message ("Taking off starts")
     self.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
@@ -47,14 +94,16 @@ def arm_and_takeoff(self, aTargetAltitude):
     # check that arrived by reading the alitude relative from the ground 
     while True:
         print (" Altitude: ", self.location.global_relative_frame.alt)
+        write_log_message (f" Altitude: {self.location.global_relative_frame.alt}")
         #Break and return from function just below target altitude.
         if self.location.global_relative_frame.alt>=aTargetAltitude*0.95:  # arrived to 95% of the altitude s
             print ("Reached target altitude")
+            write_log_message ("Reached target altitude")
             break
         time.sleep(1)
 
-
 def calculate_relative_pos(self):
+    write_log_message (f"{get_current_function_name()} called:")
     relative_x=0
     relative_y=0
     relative_z=0
@@ -73,10 +122,10 @@ def calculate_relative_pos(self):
         relative_z += current_alt - home_alt
         time.sleep(0.1)
         # Print the drone's position relative to the home position
-    print(f"Position: ({relative_x/3:.2f}, {relative_y/3:.2f}, {relative_z/3:.2f})")
+        return [relative_x/3,relative_y/3, relative_z/3]
 
 def send_ned_velocity(self, velocity_x, velocity_y, velocity_z, duration):
-
+    write_log_message (f"{get_current_function_name()} called:")
     msg = self.message_factory.set_position_target_local_ned_encode(
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
@@ -92,17 +141,14 @@ def send_ned_velocity(self, velocity_x, velocity_y, velocity_z, duration):
     #     self.send_mavlink(msg)
     #     #calculate_relative_pos()
     #     time.sleep(1)
-
+    # more accurate than using for loop and sleep because duration could be a float number 
     start_time = time.time()
     while time.time() - start_time < duration:
         self.send_mavlink(msg)
         time.sleep(0.1)
 
-
-
-
 def send_ned_velocity_stages(self, velocity_x, velocity_y, velocity_z, duration):
-
+    write_log_message (f"{get_current_function_name()} called:")
     stages = 6  # Number of stages
 
     # Calculate distance and time for each stage
@@ -125,14 +171,21 @@ def send_ned_velocity_stages(self, velocity_x, velocity_y, velocity_z, duration)
             stage_speed_y = velocity_y * 0.5
             stage_speed_z = velocity_z * 0.5
 
-        # Calculate duration for the current stage
-        if stage == 0 or stage == stages - 1:  # First and last stage (25% and 25%)
-            stage_time = stage_duration * 4.0 # since it speed of 25% then you need 4 times the duration of the satge to go the distance 
-        elif stage ==2 or stage ==3:  # Middle stage (100%)
-            stage_time = stage_duration
-        else:  # Intermediate stages (50%)
-            stage_time = stage_duration *2 # same herer you need 2 times the duration of the stage since you need only 50% of speed so double the time 
+        # # Calculate duration for the current stage
+        # if stage == 0 or stage == stages - 1:  # First and last stage (25% and 25%)
+        #     stage_time = stage_duration * 4.0 # since it speed of 25% then you need 4 times the duration of the satge to go the distance 
+        # elif stage ==2 or stage ==3:  # Middle stage (100%)
+        #     stage_time = stage_duration
+        # else:  # Intermediate stages (50%)
+        #     stage_time = stage_duration *2 # same herer you need 2 times the duration of the stage since you need only 50% of speed so double the time 
 
+        # Calculate duration for the current stage faster 
+        if stage == 0 or stage == stages - 1:  # First and last stage (25% and 25%)
+            stage_time = stage_duration * 2.0 # since it speed of 25% then you need 4 times the duration of the satge to go the distance 
+        elif stage ==2 or stage ==3:  # Middle stage (100%)
+            stage_time = stage_duration*2
+        else:  # Intermediate stages (50%)
+            stage_time = stage_duration *1 # same herer you need 2 times the duration of the stage since you need only 50% of speed so double the time 
 
         # Create and send the MAVLink message for the current stage
         msg = self.message_factory.set_position_target_local_ned_encode(
@@ -164,6 +217,7 @@ def send_ned_velocity_stages(self, velocity_x, velocity_y, velocity_z, duration)
 
 # this function will calculate the speed and the time and call send_ned_velocity 
 def move_to(self, x , y): 
+    write_log_message (f"{get_current_function_name()} called:")
     # need to set speed constant and we change the time 
     # the north is the y access in the calaulation of the hex and the east is the x 
     # example reived x=a, y=b 
@@ -179,8 +233,8 @@ def move_to(self, x , y):
     #send_ned_velocity_stages(self,north/float(total_time) ,east/float(total_time), down, total_time)
     # you can not have one speed for both and only one time for both , so we fix time and change the speed 
 
-
 def move_to_stages(self, x , y): 
+    write_log_message (f"{get_current_function_name()} called:")
     # need to set speed constant and we change the time 
     # the north is the y access in the calaulation of the hex and the east is the x 
     # example reived x=a, y=b 
@@ -196,8 +250,8 @@ def move_to_stages(self, x , y):
     send_ned_velocity_stages(self,north/float(total_time) ,east/float(total_time), down, total_time)
     # you can not have one speed for both and only one time for both , so we fix time and change the speed 
 
-
 def set_home_to_zero(self):
+    write_log_message (f"{get_current_function_name()} called:")
 
     home_position_cmd = self.message_factory.command_long_encode(
     0, 0,                             # target system, target component
@@ -209,8 +263,8 @@ def set_home_to_zero(self):
     )
     self.send_mavlink(home_position_cmd)
 
-
 def min_time_safe():
+    write_log_message (f"{get_current_function_name()} called:")
     max_speed = 2  # Maximum speed in m/s
     longest_duration = 0  # Initialize the longest duration as 0
 
