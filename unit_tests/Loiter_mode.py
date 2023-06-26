84e0529f56971fef1e5cf0acebc4aa90a1b5383c
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-06-26 17:34:30 +0200

    change to land intead of RTL

--- unit_tests/Loiter_mode.py
+++ unit_tests/Loiter_mode.py
@@ -14,17 +14,18 @@ import math
 
 create_log_file(os.path.dirname(os.path.abspath(__file__)),  os.path.splitext(os.path.basename(__file__))[0]) 
 
-vehicle = connect(parse_connect(), wait_ready=False)
-#set_home_to_zero(vehicle)
-arm_and_takeoff(vehicle, 10)
+vehicle = connect(parse_connect(), wait_ready=False,baud=57600)
+vehicle.wait_ready(True, raise_exception=False)
+
+arm_and_takeoff(vehicle, 0.5)
 
 write_log_message(f" current_altitude= {vehicle.location.global_relative_frame.alt}")
 vehicle.mode    = VehicleMode("LOITER") #loiter mode and hover in your place 
 write_log_message(f" current_altitude= {vehicle.location.global_relative_frame.alt}")
 time.sleep(10)
 
-write_log_message(f" Coming Home")
-vehicle.mode = VehicleMode ("RTL")
+write_log_message(f" LAND")
+vehicle.mode = VehicleMode ("LAND")
 time.sleep(3) 
 
 vehicle.close()
