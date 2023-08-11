edb66366ef7293fc0937ec3aff04aacf945ed579
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-08-11 13:03:07 +0200

    increase hz of retrieving data

--- unit_tests/another_pid.py
+++ unit_tests/another_pid.py
@@ -19,46 +19,51 @@ def interrupt(signal_num, frame):
     print("Interrupted!")
     global vehicle
     if vehicle is not None:
+        vehicle.remove_attribute_listener('velocity', on_velocity)
         vehicle.mode = VehicleMode ("LAND")
         time.sleep(3) 
         vehicle.close()
         sys.exit()
 
+# def on_velocity(self, attribute_name, value):
+#     print("Velocity: %s" % str(value))
+
 create_log_file(os.path.dirname(os.path.abspath(__file__)),  os.path.splitext(os.path.basename(__file__))[0]) 
 global vehicle
-#vehicle = connect(parse_connect(), wait_ready=False)
-vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False)
-vehicle.wait_ready(True, raise_exception=False)
+vehicle = connect(parse_connect(), wait_ready=False, rate=50)
+# vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False)
+# vehicle.wait_ready(True, raise_exception=False)
 signal.signal(signal.SIGINT, interrupt)
 
 set_a(3)
+# vehicle.add_attribute_listener('velocity', on_velocity)
 
 drone= Drone(0.0,0.0,2) # drone at the sink 
 #set_home_to_zero(vehicle)
 arm_and_takeoff(vehicle,drone.hight)
 print( "Takeoff and wait 2 sec")
 time.sleep(2)
-vehicle.mode    = VehicleMode("LOITER") #loiter mode and hover in your place 
+# vehicle.mode    = VehicleMode("LOITER") #loiter mode and hover in your place 
 time.sleep(2)
 vehicle.mode     = VehicleMode("GUIDED")
 # face_north(vehicle)
 
-distance=4
-time_to_pass=8
+distance=8
 angl_dir= 90#-45
 # angle dir in degree 
-#move_PID_body(vehicle,angl_dir, distance, time_to_pass)
-move_PID_body_manual(vehicle, drone.hight, angl_dir, distance, time_to_pass)
+#move_PID_body(vehicle, drone.hight, angl_dir, distance)
+move_PID_body_manual(vehicle, drone.hight, angl_dir, distance)
 
-vehicle.mode    = VehicleMode("LOITER") #loiter mode and hover in your place 
-time.sleep(3)
+# vehicle.mode    = VehicleMode("LOITER") #loiter mode and hover in your place 
 vehicle.mode     = VehicleMode("GUIDED")
 
 print( "move to the other direction")
+time.sleep(5)
+
 angl_dir= -90#135
 # angle dir in degree 
-#move_PID_body(vehicle, angl_dir, distance,time_to_pass)
-move_PID_body_manual(vehicle,drone.hight,  angl_dir, distance, time_to_pass)
+#move_PID_body(vehicle, drone.hight, angl_dir, distance)
+move_PID_body_manual(vehicle,drone.hight,  angl_dir, distance)
 
 
 write_log_message(f" Coming Home")
