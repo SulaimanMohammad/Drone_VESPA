5f4635ea8ee6f849e1d870b9e165c070876c1616
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-08-18 16:16:06 +0200

    listen to gps data

--- unit_tests/another_pid.py
+++ unit_tests/another_pid.py
@@ -25,18 +25,28 @@ def interrupt(signal_num, frame):
         vehicle.close()
         sys.exit()
 
+# Define a listener function to receive updates
+def attribute_listener(self, attr_name, value):
+    if attr_name == 'location.global_frame':
+        latitude = value.lat
+        longitude = value.lon
+        altitude = value.alt
+        print(f"Latitude: {latitude}, Longitude: {longitude}, Altitude: {altitude}")
+
 # def on_velocity(self, attribute_name, value):
 #     print("Velocity: %s" % str(value))
 
 create_log_file(os.path.dirname(os.path.abspath(__file__)),  os.path.splitext(os.path.basename(__file__))[0]) 
 global vehicle
-#vehicle = connect(parse_connect(), wait_ready=False, rate=10)
-vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False, rate=10)
-vehicle.wait_ready(True, raise_exception=False)
+vehicle = connect(parse_connect(), wait_ready=False, rate=10)
+# vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False, rate=10)
+# vehicle.wait_ready(True, raise_exception=False)
 signal.signal(signal.SIGINT, interrupt)
 
 set_a(3)
 # vehicle.add_attribute_listener('velocity', on_velocity)
+# Add the listener to the vehicle's attribute
+vehicle.add_attribute_listener('location.global_frame', attribute_listener)
 
 drone= Drone(0.0,0.0,2) # drone at the sink 
 #set_home_to_zero(vehicle)
@@ -70,6 +80,7 @@ move_PID_body_manual(vehicle,drone.hight,  angl_dir, distance)
 write_log_message(f" Coming Home")
 vehicle.mode = VehicleMode ("LAND")
 time.sleep(2) 
+vehicle.remove_attribute_listener('location.global_frame', attribute_listener)
 
 # Close connection
 vehicle.close()
