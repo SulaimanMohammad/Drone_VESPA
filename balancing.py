00a2f393cfdd3be867aae431f19cd542e881edbd
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-09-29 22:56:30 +0200

    border spot waiting messages and timer

--- balancing.py
+++ balancing.py
@@ -1,5 +1,49 @@
 from expan import *
 
+class Boarder_Timer:
+    def __init__(border_t,self,timeout=2*movement_time):
+        border_t.timeout = timeout
+        border_t.message_counter = 0  # Initialize message count
+        border_t.remaining_time = border_t.timeout
+        #border_t.lock_sink = threading.Lock()  # Create a lock
+        border_t.message_thread = threading.Thread(target=border_listener, args=(border_t,self,timeout,))
+        border_t.message_thread.start()
+        border_t.end_of_balancing= threading.Event()
+
+    def run(border_t, self):
+        while True:
+            with border_t.lock_sink:  # Acquire the lock
+                border_t.remaining_time -= 0.5
+                print(f"Remaining time: {border_t.remaining_time:.2f} seconds")
+                if border_t.remaining_time <= 0:
+                    border_t.time_up(border_t,self)
+                    break
+            time.sleep(0.5)
+
+    def time_up(border_t,self):
+        #Called when the timer reaches its timeout without being reset.
+        print("Time's up! ")
+        
+        if border_t.message_counter==0: #if no message received 
+            pass
+        else: 
+            border_t.end_of_balancing.set() # flag to tell listener that it is the end 
+            border_listener.join() # stop listenning 
+
+
+def border_listener(self,border_t, timeout):
+    while not border_t.end_of_balancing.is_set(): # the end is not reached , keep listenning 
+        msg= self.retrive_msg_from_buffer() 
+        if msg.startswith(Arrival_header.encode()) and msg.endswith(b'\n'):
+            print("Message received!")
+            positionX, positionY, state, id_value= self.decode_spot_info_message(msg)
+            self.update_neighbors_list(positionX, positionY, state, id_value )
+            border_t.message_counter += 1
+            border_t.remaining_time = border_t.timeout
+    
+    border_t.end_of_balancing.clear() # reset so the next spanning the listenning loop will be activated  
+
+
 def communication_balancing(self):
     msg= self.retrive_msg_from_buffer() 
     # Receiving message asking data 
@@ -12,6 +56,7 @@ def communication_balancing(self):
         self.neighbors_list_updated = threading.Event()
         positionX, positionY, state, id_value= self.decode_spot_info_message(msg)
         self.update_neighbors_list(positionX, positionY, state, id_value )
+
         self.neighbors_list_updated.set()
 
 
@@ -61,10 +106,15 @@ def construct_allowed_spots(self, border_neighbors ):
 
 
 def balancing(self, vehicle):
-    self.xbee_receive_message_thread = threading.Thread(target=self.communication_balancing, args=(self,)) #pass the function reference and arguments separately to the Thread constructor.
-    self.xbee_receive_message_thread.start()
-    brder_found= False
+    if self.state== Border:
+        border_process=Boarder_Timer()
+        border_process.run()
+
+    
     if self.state == Free: 
+        self.xbee_receive_message_thread = threading.Thread(target=self.communication_balancing, args=(self,)) #pass the function reference and arguments separately to the Thread constructor.
+        self.xbee_receive_message_thread.start()
+        brder_found= False
         while border_found == False : # no border in the nieghbor
             self.build_data_demand_message()
             self.neighbors_list_updated.wait()
@@ -72,7 +122,9 @@ def balancing(self, vehicle):
             border_found, spot_to_go = search_going_to_border(self)
             # Move             
             self.move_to_spot(vehicle, spot_to_go)
-
+        # This message will be read by the border drone and its niegbor
+        data_msg= self.build_spot_info_message(Arrival_header)
+        self.send_msg(data_msg)
 
 
 
