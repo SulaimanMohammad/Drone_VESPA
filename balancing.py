f320835ce787fc755a9da1170b21fa7fdd7d69fa
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-10-02 17:06:05 +0200

    local balancing methode

--- balancing.py
+++ balancing.py
@@ -3,7 +3,6 @@ from expan import *
 class Boarder_Timer:
     def __init__(border_t,self,timeout=2*movement_time):
         border_t.timeout = timeout
-        border_t.message_counter = 0  # Initialize message count
         border_t.remaining_time = border_t.timeout
         #border_t.lock_sink = threading.Lock()  # Create a lock
         border_t.message_thread = threading.Thread(target=border_listener, args=(border_t,self,timeout,))
@@ -24,29 +23,74 @@ class Boarder_Timer:
         #Called when the timer reaches its timeout without being reset.
         print("Time's up! ")
         
-        if border_t.message_counter==0: #if no message received 
-            pass
-        else: 
-            border_t.end_of_balancing.set() # flag to tell listener that it is the end 
-            border_listener.join() # stop listenning 
+        border_t.local_balancing.set() # flag to tell listener that it is the end
+        
+        # border_listener.join() # stop listenning 
 
 
 def border_listener(self,border_t, timeout):
     while not border_t.end_of_balancing.is_set(): # the end is not reached , keep listenning 
         msg= self.retrive_msg_from_buffer() 
+        
+        # if a drone asked the info it shouls be send, but no need to recive respond 
+        if msg == Demand_header.encode() + b'\n':
+            data_msg= self.build_spot_info_message(Reponse_header) # Build message that contains all data
+            self.send_msg(data_msg)
+
         if msg.startswith(Arrival_header.encode()) and msg.endswith(b'\n'):
             print("Message received!")
+            # This can be recived in case of drone arrive to the current spot or another border neigbors
             positionX, positionY, state, id_value= self.decode_spot_info_message(msg)
             self.update_neighbors_list(positionX, positionY, state, id_value )
-            border_t.message_counter += 1
-            border_t.remaining_time = border_t.timeout
-    
+            border_t.remaining_time = border_t.timeout     
+            id, destination= self.lead_local_balancing()
+            self.build_local_movement_message(id, destination)
+
+
+
+
     border_t.end_of_balancing.clear() # reset so the next spanning the listenning loop will be activated  
 
 
-def communication_balancing(self):
+def build_local_movement_message(self, target_id, destination):
+
+    # Determine max byte count for numbers
+    max_byte_count = self.determine_max_byte_size(target_id)
+    
+    # Start the message with the header and the max byte count for target_id
+    message = Local_balance_header.encode() + struct.pack('>B', max_byte_count)
+    
+    # Add the target_id to the message using the determined byte count
+    message += target_id.to_bytes(max_byte_count, 'big')
+    
+    # Append the destination to the message ( 1 bute becausee it is spot 0-6 )
+    message += destination.to_bytes(1, 'big')
+    
+    # End with '\n'
+    message += b'\n'
+    
+    return message
+
+def decode_local_movement_message(self, message):
+
+    # Extract max byte count for target_id
+    max_byte_count = struct.unpack('>B', message[1:2])[0]  # Dereferenced the tuple to get the value
+    
+    # Extract target_id using max byte count
+    target_id_start = 2
+    target_id_end = target_id_start + max_byte_count
+    target_id = int.from_bytes(message[target_id_start:target_id_end], 'big')
+    
+    # Extract the destination
+    destination = int.from_bytes(message[target_id_end:target_id_end+1], 'big')  # Since destination is always one byte
+    
+    return target_id, destination
+
+def communication_balancing(self,vehicle):
+    
     msg= self.retrive_msg_from_buffer() 
-    # Receiving message asking data 
+    
+    # Receiving message asking for data 
     if msg == Demand_header.encode() + b'\n':
         data_msg= self.build_spot_info_message(Reponse_header) # Build message that contains all data
         self.send_msg(data_msg)
@@ -59,6 +103,37 @@ def communication_balancing(self):
 
         self.neighbors_list_updated.set()
 
+    # Recieve message from the border to move 
+    if msg.startswith(Local_balance_header.encode()) and msg.endswith(b'\n'):
+        rec_id,destination= self.decode_local_movement_message(msg)
+        if rec_id == self.id: # Current drone is targeted
+            self.move_to_spot(vehicle, destination)
+            data_msg= self.build_spot_info_message(Arrival_header)
+            self.send_msg(data_msg)
+
+def lead_local_balancing(self):
+    # Extract the 'free' drone IDs in s0
+    s0 = next(station for station in self.neighbor_list if station['name'] == 's0')
+    s0_free_ids = sorted([s0['drones_in_id'][idx] for idx, state in enumerate(s0['states']) if state == Free])
+    
+    # Identify all stations with the "border" state
+    border_stations = [station for station in  self.neighbor_list if Border or Irremovable_boarder in station['states']]
+    
+    moves = []
+    
+    # For each "border" station, calculate the difference in the number of "free" states with s0
+    for station in border_stations:
+        station_free_count = sum(1 for state in station['states'] if state == Free)
+        diff = len(s0_free_ids) - station_free_count
+        
+        # If s0 has more 'free' drones than the "border" station by more than 1, move the smallest "free" drone ID
+        while diff > 1 and s0_free_ids:
+            drone_id_to_move = s0_free_ids.pop(0)  # Get the smallest "free" drone ID
+            destination_number = station['name'][1:]  # Extract the number after "s"
+            moves.append((drone_id_to_move, destination_number))
+            diff -= 1
+    
+    return moves
 
 def search_going_to_border(self):
     border_found=False
@@ -90,7 +165,6 @@ def search_going_to_border(self):
         max_distance_spot = max(self.neighbor_list, key=lambda x: x['distance'])
         return border_found, int(max_distance_spot['name'][1:]) 
 
-
 # the border is the chef of the spot 
 # end when a message from each border to another that balancig is done 
 
@@ -112,7 +186,7 @@ def balancing(self, vehicle):
 
     
     if self.state == Free: 
-        self.xbee_receive_message_thread = threading.Thread(target=self.communication_balancing, args=(self,)) #pass the function reference and arguments separately to the Thread constructor.
+        self.xbee_receive_message_thread = threading.Thread(target=self.communication_balancing, args=(self,vehicle,)) #pass the function reference and arguments separately to the Thread constructor.
         self.xbee_receive_message_thread.start()
         brder_found= False
         while border_found == False : # no border in the nieghbor
