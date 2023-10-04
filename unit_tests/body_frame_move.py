51471974bf0831b9b6fc199092418de71d58f7f2
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-10-04 16:50:51 +0200

    no rate defined

--- unit_tests/body_frame_move.py
+++ unit_tests/body_frame_move.py
@@ -27,7 +27,7 @@ create_log_file(os.path.dirname(os.path.abspath(__file__)),  os.path.splitext(os
 global vehicle
 
 #vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
-vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False,rate=10) # for raspberry pi
+vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False) # for raspberry pi
 #vehicle = connect("/dev/ttyUSB0", baud= 57600,  wait_ready=False, rate=10) #for telemetry 
 vehicle.wait_ready(True, raise_exception=False) # for raspberry pi & telemetry only 
 signal.signal(signal.SIGINT, interrupt)
