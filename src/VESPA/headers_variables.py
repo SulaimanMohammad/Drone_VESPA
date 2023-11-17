8d332a8855e08b20877d74ce528999b5a6a4239e
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2023-11-17 12:10:45 +0100

    set_env() to load variables and module to the caller script global

---
+++ src/VESPA/headers_variables.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import re
+import importlib
+
+'''
+These will be used in the building and decoding message
+'''
+#Exchanging data Headers
+Movement_command= 'M'
+Calibration= 'C'
+Demand_header= 'D'
+Response_header= 'R'
+ACK_header= "K"
+# Expansion Headers
+Expan_header= 'E'
+Arrival_header= 'A'
+Inherit_header= 'I'
+Forming_border_header= 'F'
+#Spanning Headers
+Spanning_header= 'S'
+Target_coordinates_header= 'T'
+# Blancing Headers
+Local_balance_header='L'
+Guidance_header= 'G'
+Balance_header= 'B'
+Algorithm_termination_header="O"
+
+# Create an array of headers
+headers = [
+    Movement_command, Calibration, Demand_header, Response_header,ACK_header,
+    Expan_header, Arrival_header, Inherit_header, Forming_border_header,
+    Spanning_header, Target_coordinates_header, Local_balance_header,
+    Guidance_header, Balance_header, Algorithm_termination_header
+]
+
+# Generate an array of ASCII values to be used in retiveing messages 
+headers_ascii_values = [ord(header) for header in headers]
+
+def read_vars_from_file():
+        # Current script directory
+        current_dir = Path(__file__).resolve().parent
+        # Path to the parent directory
+        parent_dir = current_dir.parent
+        global Operational_Data_path
+        Operational_Data_path = parent_dir/'Operational_Data.txt'
+
+        with open(Operational_Data_path, 'r') as file:
+            for line in file:
+                match = re.match(r"(\w+)\s*=\s*([^#]+)", line)
+                if match:
+                    var_name, value = match.groups()
+                    if var_name in ["max_acceleration", "max_deceleration"]:
+                        continue
+                    else:
+                        try:
+                            global_vars[var_name] = eval(value.strip(), {}, global_vars)
+                        except NameError:
+                            global_vars[var_name] = value.strip()
+
+global_vars = {}
+def load_vars():
+    read_vars_from_file() # Include set id and xbee range "a" 
+    globals().update(global_vars)
+
+def get_global_vars():
+    return global_vars
+
+def get_xbee_module():
+    if Tx==None and Rx== None:
+        module = importlib.import_module('Xbee_module.xbee_usb')
+    else:
+        module = importlib.import_module('Xbee_module.xbee_pigpio')
+    return module 
+
+def set_env(globals_dict):
+    load_vars()
+    global_vars = get_global_vars()
+    xbee_module = get_xbee_module()
+    globals_dict.update(global_vars)
+    globals_dict.update(xbee_module.__dict__)
