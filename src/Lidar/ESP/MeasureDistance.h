d36f3943e1f6162f3e29913cc9cfd97ab1fe7e50
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-01-21 14:40:16 +0100

    get the distance from sensor header and implementation

---
+++ src/Lidar/ESP/MeasureDistance.h
@@ -0,0 +1,9 @@
+#ifndef MEASUREDISTANCE_H
+#define MEASUREDISTANCE_H
+#include "Characterization.h"
+#include "ReadTOF.h"
+
+int calculateMedianDistance(int samples[]);
+void sortArray(int arr[], int numElements);
+int get_distance();
+#endif // MEASUREDISTANCE_H
