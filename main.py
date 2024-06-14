e9b0d8ff3092d92bc01a539dbca5ef4b393d7b09
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-14 21:53:13 +0200

    Serail connection's established in drone constructor

--- main.py
+++ main.py
@@ -25,11 +25,6 @@ vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False) # for raspber
 vehicle.wait_ready(True, raise_exception=False) 
 set_data_rate(vehicle, 20)
 
-# Serial port settings for XBee
-serial_port = '/dev/ttyUSB0'  # Default UART port on Raspberry Pi
-baud_rate = 9600  # Baud rate for XBee
-connect_xbee(serial_port, baud_rate) 
-
 # Create drone object of VESPA 
 drone = Drone(0,0.0,0.0)
 # Configure parameter of drone based on VESPA
