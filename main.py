e5da3c7d07aaba0e73907b32ec62704ab83ceb29
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-14 17:31:20 +0200

    update import in main

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-
 import sys
 import os
 
-parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), './src'))
 # Add the parent directory to sys.path
 sys.path.append(parent_directory)
-from Xbee_module.xbee_usb import connect_xbee,close_xbee_port
+from Xbee_module.xbee_pigpio import connect_xbee,close_xbee_port
+parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), './src'))
+# Add the parent directory to sys.path
+sys.path.append(parent_directory)
 
 from VESPA.VESPA_module import *
 from VESPA.expansion import first_exapnsion,further_expansion
-from VESPA.spanning import spanning
-from VESPA.balancing import balancing
 
-parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), './src'))
 # Add the parent directory to sys.path
 sys.path.append(parent_directory)
 from drone.set_drone_parameters import * 
@@ -26,12 +26,13 @@ vehicle.wait_ready(True, raise_exception=False)
 set_data_rate(vehicle, 20)
 
 # Serial port settings for XBee
-serial_port = '/dev/ttyUSB0'  # Default UART port on Raspberry Pi
-baud_rate = 9600  # Baud rate for XBee
-connect_xbee(serial_port, baud_rate) 
+#serial_port = '/dev/ttyUSB0'  # Default UART port on Raspberry Pi
+#baud_rate = 9600  # Baud rate for XBee
+#connect_xbee(serial_port, baud_rate) 
 
 # Create drone object of VESPA 
 drone = Drone(0,0.0,0.0)
+print("id=", drone.id)
 # Configure parameter of drone based on VESPA
 config_parameters(vehicle, drone)
 signal.signal(signal.SIGINT, lambda sig, frame: drone.interrupt(vehicle))
@@ -46,10 +47,3 @@ finally:
     close_xbee_port()
     vehicle.close()
     print("Serial connection closed.")
-
-
-
-
-
-
-
