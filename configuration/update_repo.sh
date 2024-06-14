6cdcf81fe1f55d4e2d1d78266a86be4565b6b77c
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-14 21:50:03 +0200

    New clone need to reset Operation_data to set the id

--- configuration/update_repo.sh
+++ configuration/update_repo.sh
@@ -5,49 +5,25 @@ REPO_URL="https://github.com/SulaimanMohammad/Drone_VESPA.git"
 PI_DRONE_DIR=$(ls /home | grep drone)
 CLONE_PATH="/home/$PI_DRONE_DIR/Drone_VESPA"
 
-
-cp /home/$PI_DRONE_DIR/Drone_VESPA/configuration/update_repo.sh /home/$PI_DRONE_DIR
-
-cd  /home/$PI_DRONE_DIR/
-
 # If the branch name is passed as an argument, use it; otherwise, default to "main".
 BRANCH_NAME=${1:-"main"}
 
-# Navigate to the directory where your repository is located
-cd /home/$PI_DRONE_DIR/Drone_VESPA
-
-# Check if the repository is already cloned
-if [ ! -d "$CLONE_PATH" ]; then
-    #git clone "$REPO_URL" "$CLONE_PATH"
-    git clone -b "$BRANCH_NAME" "$REPO_URL" "$CLONE_PATH"
-
+# Check if the repository is already cloned and remove it if it exists
+if [ -d "$CLONE_PATH" ]; then
+    echo "Repository exists. Removing..."
+    rm -rf "$CLONE_PATH"
 fi
+
+# Clone the repository
+git clone -b "$BRANCH_NAME" "$REPO_URL" "$CLONE_PATH"
 
 # Adjust the ownership:
 sudo chown -R $USER:$USER "$CLONE_PATH"
 
+cp /home/$PI_DRONE_DIR/Drone_VESPA/configuration/update_repo.sh /home/$PI_DRONE_DIR
+cd  /home/$PI_DRONE_DIR/
 
 # Navigate to the directory
 cd "$CLONE_PATH" || exit # prevent the script from continuing if it can't enter the desired directory
 
-
-# Configure Git to use the merge strategy for pulling
-git config --global pull.rebase false
-
-# Fetch updates from the remote repository
-git fetch
-
-# Check if there are any updates
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-    git pull
-else
-    echo "Diverged"
-fi
+./configuration/setup_drone_info.sh 
