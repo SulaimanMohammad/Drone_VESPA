c6d1601855faed08bc8e3ce29a7473247873592e
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-17 12:54:28 +0200

    realease lock to avoid bloking thread to jon

--- src/VESPA/VESPA_module.py
+++ src/VESPA/VESPA_module.py
@@ -882,8 +882,6 @@ class Drone:
             if not self.Emergency_stop.is_set():
                 self.Emergency_stop.set()
                 self.expansion_stop.set()
-                self.exchange_data_lock.release() # dont allow excahnge msg and rest the list 
-                self.lock_neighbor_list.release()
                 emergency_msg= self.build_emergency_message()
                 send_msg(emergency_msg)
                 print("retuen home")
@@ -903,6 +901,8 @@ class Drone:
                         continue  # Skip the main thread
                     print(f"Joining thread: {thread.name or 'Unnamed'}, ID: {thread.ident}")
                     print(not self.Forming_Border_Broadcast_REC.is_set() , (not self.expansion_stop.is_set()) , (not self.Emergency_stop.is_set()))
+                    self.exchange_data_lock.release() # dont allow excahnge msg and rest the list 
+                    self.lock_neighbor_list.release()
                     thread.join()
                     print("joined")
                 print("All threads have been joined.")
