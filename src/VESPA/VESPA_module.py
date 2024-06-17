a021d7e3bbcc18dc3b5b68e07529d920411cc65f
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-17 14:43:56 +0200

    join all except te deamon

--- src/VESPA/VESPA_module.py
+++ src/VESPA/VESPA_module.py
@@ -271,10 +271,11 @@ class Drone:
         with self.exchange_data_lock: 
             self.list_finished_update.clear()
             copy_neighbor_list= copy.deepcopy(self.neighbor_list) # use deepcopy or it will be reference not copy 
+            print(" finished deep copy")
             recollect_data=0
             self.rest_neighbor_list()
             reseted_neighbor_list= copy.deepcopy(self.neighbor_list) 
-            
+            print(" finished reset list ")
             while (self.compare_with_neighbor_list(reseted_neighbor_list,'drones_in')) and (recollect_data<2) and (not self.Emergency_stop.is_set()): 
                 demand_msg= self.build_data_demand_message()
                 send_msg(demand_msg)
@@ -282,7 +283,8 @@ class Drone:
                 recollect_data= recollect_data +1
                 time.sleep(exchange_data_latency)
                 print(" still readig in demand")
-
+                
+            print(" finished collecting data ")
             if self.resposnse_rec_counter==0: # No response recieved so it is blocked thread restor the old list 
                 with self.lock_neighbor_list:
                   self.neighbor_list=  copy.deepcopy(copy_neighbor_list) 
