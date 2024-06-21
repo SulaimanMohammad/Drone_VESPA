20b1673349e312442f575cb9214564e8e3c113d3
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-21 09:42:36 +0200

    station

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 
@@ -12,56 +11,36 @@ sys.path.append(parent_directory)
 
 from VESPA.VESPA_module import *
 from VESPA.expansion import first_exapnsion,further_expansion
-from VESPA.spanning import spanning
-from VESPA.balancing import balancing
 
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), './src'))
 # Add the parent directory to sys.path
 sys.path.append(parent_directory)
 from drone.set_drone_parameters import * 
 
-# Create vehicle object 
-logs= create_log_file() 
-#vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
-vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False) # for raspberry pi
-#vehicle = connect("/dev/ttyUSB0", baud= 57600,  wait_ready=False, rate=10) #for telemetry 
-vehicle.wait_ready(True, raise_exception=False) 
-set_data_rate(vehicle, 20)
-
-# Create drone object of VESPA 
-drone = Drone(0,0.0,0.0)
-# Configure parameter of drone based on VESPA
-config_parameters(vehicle, drone)
-signal.signal(signal.SIGINT, lambda sig, frame: drone.interrupt(vehicle))
-
-time.sleep(2) 
-
-try:
-    first_exapnsion(drone, vehicle)
-    spanning(drone)
-    balancing(drone, vehicle)
-
-    while drone.check_termination():
-        further_expansion( drone, vehicle)
-        spanning(drone,vehicle)
-        # Since the irremovable drone as stuck in spanning, then it will retuen and try to performe balancing 
-        if drone.state==Irremovable and drone.VESPA_termination.is_set():
-            break
-        # NO need to do same for border because VESPA_termination will be set in balancing and then while evaluation comes
-        balancing(drone, vehicle )
-
-except:
-    print("Error in performing VESPA")
-    drone.emergency_stop()
-finally:
-    drone.return_home(vehicle) 
-    close_xbee_port()
-    vehicle.close()
-    print("Serial connection closed.")
+def main():
+    # Create vehicle object 
+    logs= create_log_file() 
+    vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
+    #vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False) # for raspberry pi
+    #vehicle = connect("/dev/ttyUSB0", baud= 57600,  wait_ready=False, rate=10) #for telemetry 
+    #vehicle.wait_ready(True, raise_exception=False) 
+    set_data_rate(vehicle, 20)
+    # Create drone object of VESPA 
+    drone = Drone(0,0.0,0.0)
+    # Configure parameter of drone based on VESPA
+    config_parameters(vehicle, drone)
+    signal.signal(signal.SIGINT, lambda sig, frame: drone.interrupt(vehicle))
+    try:
+        first_exapnsion(drone, vehicle)
+    except:
+        print("Error in performing VESPA")
+        drone.emergency_stop()
+    finally:
+        drone.return_home(vehicle) 
+        close_xbee_port()
+        vehicle.close()
+        print("Serial connection closed.")
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    main()
