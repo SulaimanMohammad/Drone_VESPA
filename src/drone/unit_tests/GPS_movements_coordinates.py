202b2d32fbb523f12f000d0c9126b4a581c6efd8
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-26 13:39:52 +0200

    unit test

--- src/drone/unit_tests/GPS_movements_coordinates.py
+++ src/drone/unit_tests/GPS_movements_coordinates.py
@@ -28,8 +28,8 @@ def interrupt(signal_num, frame):
 create_log_file() 
 global vehicle
 
-#vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
-vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False,rate=10) # for raspberry p
+vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
+#vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False,rate=10) # for raspberry p
 #vehicle = connect("/dev/ttyUSB0", baud= 57600,  wait_ready=False, rate=10) #for telemetry
 vehicle.wait_ready(True, raise_exception=False) # for raspberry pi & telemetry only 
 signal.signal(signal.SIGINT, interrupt)
@@ -40,8 +40,8 @@ print( "Takeoff and wait 2 sec")
 time.sleep(2)
 
 defined_groundspeed=1
-distance=10
-angl_dir= 90
+distance=13
+angl_dir= 0
 
 current_lat = vehicle.location.global_relative_frame.lat
 current_lon = vehicle.location.global_relative_frame.lon
@@ -50,22 +50,13 @@ long, lat= new_coordinates(current_lon, current_lat, distance, angl_dir)
 point1 = LocationGlobalRelative( lat,long,drone_hight)
 vehicle.simple_goto( point1, groundspeed=defined_groundspeed)
 # simple_goto will retuen after the command is sent, thus you need to sleep to give the drone time to move 
-time.sleep( (distance/defined_groundspeed)+1 )
+time.sleep( (distance/defined_groundspeed)+5 )
 #loiter mode and hover in your place if it is before sleep then the drone will not move
 vehicle.mode    = VehicleMode("LOITER") #loiter mode and hover in your place 
 time.sleep(2)
 vehicle.mode     = VehicleMode("GUIDED")
-
-print( "move to the other direction")
-angl_dir= 270#135
-# angle dir in degree new_coordinates(vehicle, distance, angl_dir)
-long, lat= new_coordinates(current_lon, current_lat, distance, angl_dir)
-point2 = LocationGlobalRelative( lat,long,drone_hight)
-vehicle.simple_goto( point2, groundspeed=defined_groundspeed)
-
-time.sleep( (distance/defined_groundspeed)+1 ) 
 write_log_message(f" Coming Home")
 print( "Coming Home")
-vehicle.mode = VehicleMode ("LAND")
+vehicle.mode = VehicleMode ("RTL")
 # Close connection
 vehicle.close()
