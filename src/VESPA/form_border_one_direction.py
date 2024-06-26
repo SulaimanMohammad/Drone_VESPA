9e2d12150836e3a212d614d04ee20ab800557f40
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-26 10:07:06 +0200

    print

--- src/VESPA/form_border_one_direction.py
+++ src/VESPA/form_border_one_direction.py
@@ -157,8 +157,6 @@ def send_msg_border_until_confirmation(self,header):
         with self.candidate_to_send_lock:
             if  self.candidate_to_send :
                 candidates_to_process = list(self.candidate_to_send)
-                print(" send_msg_border_until_confirmation ")
-                print("candidate_to_send",self.candidate_to_send)
         
         if not self.Forming_Border_Broadcast_REC.is_set(): # dont reset at the end of phase since it listeners will be bloked
             self.demand_neighbors_info()
@@ -175,6 +173,7 @@ def send_msg_border_until_confirmation(self,header):
                 if self.current_target_ids is not None:
                     msg= build_border_message(self,header,self.current_target_ids, candidate) 
                     send_msg(msg)
+                    print("forwarard the candiate ",candidate , "to",  self.current_target_ids)
                     time.sleep(exchange_data_latency)# time untile the message arrives 
         time.sleep(exchange_data_latency)
         # except:
