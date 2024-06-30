113b6982c53e347048619290dae658bd023a6970
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-06-30 19:28:56 +0200

    Wait start signal to start the process

--- main.py
+++ main.py
@@ -20,46 +20,63 @@ parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), './sr
 sys.path.append(parent_directory)
 from drone.set_drone_parameters import * 
 
-# Create vehicle object 
-logs= create_log_file() 
-#vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
-vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False) # for raspberry pi
-#vehicle = connect("/dev/ttyUSB0", baud= 57600,  wait_ready=False, rate=10) #for telemetry 
-vehicle.wait_ready(True, raise_exception=False) 
-set_data_rate(vehicle, 20)
 
-# Create drone object of VESPA 
-drone = Drone(0,0.0,0.0)
-# Configure parameter of drone based on VESPA
-config_parameters(vehicle, drone)
-signal.signal(signal.SIGINT, lambda sig, frame: drone.interrupt(vehicle))
+def wait_start_signal():
+    # Dummy flag class with an is_set method needed for retrieve_msg_from_buffer
+    class waitlag:
+        def is_set():
+            return False
+    
+    start_recived=True  
+    while start_recived: 
+        msg= retrieve_msg_from_buffer(waitlag)
+        if msg.startswith(Inauguration_header.encode()) and msg.endswith(b'\n'):
+            print("Start VESPA receivd ")
+            send_msg(Inauguration_header.encode()+ b'\n') 
+            start_recived=False
+    time.sleep(2)
 
-time.sleep(2) 
+def main():
+    # Create vehicle object 
+    logs= create_log_file() 
+    #vehicle = connect (parse_connect(), wait_ready=False) # for simulation 
+    vehicle = connect("/dev/serial0", baud= 921600,  wait_ready=False) # for raspberry pi
+    #vehicle = connect("/dev/ttyUSB0", baud= 57600,  wait_ready=False, rate=10) #for telemetry 
+    vehicle.wait_ready(True, raise_exception=False) 
+    set_data_rate(vehicle, 20)
+    # Create drone object of VESPA 
+    drone = Drone(0,0.0,0.0)
+    # Configure parameter of drone based on VESPA
+    config_parameters(vehicle, drone)
+    signal.signal(signal.SIGINT, lambda sig, frame: drone.interrupt(vehicle))
 
-try:
-    first_exapnsion(drone, vehicle)
-    spanning(drone)
-    balancing(drone, vehicle)
+    wait_start_signal() # Wait the start flag to initiate VESPA
 
-    while drone.check_termination():
-        further_expansion( drone, vehicle)
-        spanning(drone,vehicle)
-        # Since the irremovable drone as stuck in spanning, then it will retuen and try to performe balancing 
-        if drone.state==Irremovable and drone.VESPA_termination.is_set():
-            break
-        # NO need to do same for border because VESPA_termination will be set in balancing and then while evaluation comes
-        balancing(drone, vehicle )
+    try:
+        first_exapnsion(drone, vehicle)
+        spanning(drone)
+        balancing(drone, vehicle)
 
-except:
-    print("Error in performing VESPA")
-    drone.emergency_stop()
-finally:
-    drone.return_home(vehicle) 
-    close_xbee_port()
-    vehicle.close()
-    print("Serial connection closed.")
+        while drone.check_termination():
+            further_expansion( drone, vehicle)
+            spanning(drone,vehicle)
+            # Since the irremovable drone as stuck in spanning, then it will retuen and try to performe balancing 
+            if drone.state==Irremovable and drone.VESPA_termination.is_set():
+                break
+            # NO need to do same for border because VESPA_termination will be set in balancing and then while evaluation comes
+            balancing(drone, vehicle )
 
+    except:
+        print("Error in performing VESPA")
+        drone.emergency_stop()
+    finally:
+        drone.return_home(vehicle) 
+        close_xbee_port()
+        vehicle.close()
+        print("Serial connection closed.")
 
+if __name__ == "__main__":
+    main()
 
 
 
