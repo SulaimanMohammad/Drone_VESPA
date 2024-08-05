bedf6d94bd71d44a599fbeead552faaf7fff77f7
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-08-05 11:18:37 +0200

    Return home, wait time proportional to id and xbee range (distance between spots)

--- src/VESPA/VESPA_module.py
+++ src/VESPA/VESPA_module.py
@@ -1016,7 +1016,7 @@ class Drone:
                 time.sleep(10) 
                 vehicle.mode = VehicleMode ("LAND")
             else:
-                time.sleep(5 * (self.id)) # Wait time proportional to the id so not all back to home at the same time 
+                time.sleep((a/defined_groundspeed)*(self.id)) # Wait time proportional to the id and xbee range( distance between spots) so not all back to home at the same time 
                 vehicle.mode = VehicleMode ("RTL")
         
     def set_thread_flags(self):
