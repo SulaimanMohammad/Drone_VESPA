a92f3f1a2e7afc61a216fad8bc63295879718f39
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-09-03 01:33:56 +0200

    change balancing termination

--- src/Xbee_module/xbee_pigpio.py
+++ src/Xbee_module/xbee_pigpio.py
@@ -144,6 +144,7 @@ def retrieve_msg_from_buffer(stop_flag):
                 complete_message = message_buffer[header_index:newline_index + 1]
                 # Remove the processed message from the buffer
                 message_buffer = message_buffer[newline_index + 1:]
+                print( "buffer message is:", message_buffer)
                 # Check if the message complies with the required format  
                 if complete_message[0] in headers_ascii_values and complete_message.endswith(b'\n'):
                     if verify_checksum( complete_message): 
