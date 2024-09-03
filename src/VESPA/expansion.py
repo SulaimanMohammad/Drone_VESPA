5792f89cd71248812fc654649114300ca16cf4e0
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-09-03 12:28:15 +0200

    change balancing termination

--- src/VESPA/expansion.py
+++ src/VESPA/expansion.py
@@ -153,6 +153,8 @@ def expansion_listener (self,vehicle):
                 #form_border_two_direction(self,Forming_border_header,msg)
             elif msg.startswith(Verify_border_header.encode()) and msg.endswith(b'\n'):
                 verify_border(self,Verify_border_header,msg)
+            else:
+                time.sleep(0.05)
         
         except:
             write_log_message("Thread expansion_listener Interrupt received, stopping...")
